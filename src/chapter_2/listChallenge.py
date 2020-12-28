f4f5b613d26a4e41db9594dffe10dca0f516bdba
Santosh Marigowda <santosh.marigowda2@sky.uk>
2020-12-28 22:54:43 +0000

    list challenge

---
+++ src/chapter_2/listChallenge.py
@@ -0,0 +1,18 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+print("end of program")
+
+print(phrase)
+print(plist)
+
+for i in range(4):
+    plist.pop()
+
+plist.pop(0)
+plist.remove("'")
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2, plist.pop(3))
+
+print(plist)
+print(''.join(plist))
+
