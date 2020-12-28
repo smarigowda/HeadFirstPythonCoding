0260dc7d19064acbc52a53ec5c79d76989c17af7
Santosh Marigowda <santosh.marigowda2@sky.uk>
2020-12-28 17:17:29 +0000

    beer song

--- src/chapter_1/date_time.py
+++ src/chapter_1/date_time.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from time import sleep
+import random
 odds = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59]
 
 # count = 1
@@ -19,3 +20,6 @@ for i in range(5):
         print("This minute seems a littile odd")
     else:
         print("Not an odd minute")
+    rand_int = random.randint(1,60)
+    print("sleeping for", rand_int, " sec")
+    sleep(random.randint(1,60))
--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-print("Hello world !!")
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.linspace(0, 20, 100)
-plt.plot(x, np.sin(x))
-plt.show()
+# print("Hello world !!")
+# import matplotlib.pyplot as plt
+# import numpy as np
+# x = np.linspace(0, 20, 100)
+# plt.plot(x, np.sin(x))
+# plt.show()
+
+for beer_num in range(99, 0, -2):
+    print(beer_num)
+
